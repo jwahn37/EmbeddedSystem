55335765f22fddda2cc24d9d9d3a319bc7ce1669
JinWoo <jwahn37@gmail.com>
2017-04-06 08:37:21 +0900

    add text editor- mode3

--- textEditor.c
+++ textEditor.c
@@ -1,10 +1,36 @@
 #include<stdio.h>
 #include "module.h"
 
-SEND_MSG textEditor(SEND_MSG sendMsg, REV_MSG revMsg)
+#define NUM_BUTTON 9
+
+int numOfInput(REV_MSG revMsg);
+
+
+//SEND_MSG textEditor(SEND_MSG sendMsg, REV_MSG revMsg)
+int main()
 {
+	SEND_MSG sendMsg, REV_MSG revMsg;
+	char switchBuf[3];
+	char alphabet[9][3]={ {'.','Q','Z'}, {'A','B','C'}, {'D','E','F'}, {'G','H','I'}, {'J','K','L'}, {'M','N','O'}, {'P','R','S'}, {'T','U','V'}, {'W','X','Y'}};
+
+	int nOfIp = nOfIp = numOfInput(revMsg);
+	int i;
+
+	if(nOfIp==1)	//only one switch occured
+	{
+		for(i=0;i<NUM_BUTTON ; i++)				
+		{
+
+	}
+}
 
 
-
+int numOfInput(REV_MSG revMsg)
+{
+	int i;
+	int nOfIp=0;
+	for(i=0;i<NUM_BUTTON;i++)
+		nOfIp+=1;
+	return nOfIp;
 
 }
